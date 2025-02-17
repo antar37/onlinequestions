bb0055dd5d2259ebb83f7baa7ac9e424920b95ec
Alex Tarasiuk <antar37@gmail.com>
2025-02-17 15:39:40 -0600

    replacing content with content.js doc

--- content.js
+++ content.js
@@ -1,4 +1,22 @@
 const content = {
   title: "Anonymous Questions",
   subtitle: "Live Anonymous Questions",
+  for_audience: "Para la Audiencia",
+  event_name: "Nombre de Evento:",
+  enter: "Entrar",
+  for_speaker: "Para el Orador",
+  admin: "Admin",
+  about: "Acerca de",
+  admin_header: "Administrador: Manejar Evento",
+  event_name_description: "Crea o abre un evento con un nombre único de evento",
+  event_password_description:
+    "Clave secreto que te identifica como administrador de este evento (no comparta con miembros de la audiencia):",
+  admin_create_button: "Crear",
+  admin_open_button: "Abrir",
+  admin_help_button: "Ayuda",
+  admin_return_button: "Regresar",
+  delete_all_questions_question:
+    "This command deletes all questions in this event. This can not be undone. Are you sure?",
+  event_title: "Preguntas anonimas: Nombre de Evento: ",
+  order_by_vote: "Ordenar por Votos",
 };
